ac8694efeff397aa0075fa97aad988261b7fab63
Anirudh Jain <anirudhjain2004@gmail.com>
2021-07-25 11:29:46 +0530

    Financials added1

--- financials.py
+++ financials.py
@@ -1,3 +1,13 @@
 from menu import getTotalSales
 from employees import getSalCost
 from inventory import getInvCost
+
+
+def getProfit():
+    return getTotalSales - getSalCost - getInvCost
+
+def getSales():
+    return getTotalSales
+
+def getCost():
+    return getInvCost + getSalCost
